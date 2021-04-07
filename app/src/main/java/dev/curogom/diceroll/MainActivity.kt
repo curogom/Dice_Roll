5ccaffd8139ee902734d8fed42ce8a6e2dd47e26
curogom <i_am@curogom.dev>
2021-04-07 21:51:19 +0900

    Image Dice

--- app/src/main/java/dev/curogom/diceroll/MainActivity.kt
+++ app/src/main/java/dev/curogom/diceroll/MainActivity.kt
@@ -3,6 +3,7 @@ package dev.curogom.diceroll
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -14,12 +15,6 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
 
         rollButton.setOnClickListener {
-//            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-//            toast.show()
-
-//            val resultTextView: TextView = findViewById(R.id.textView)
-//            resultTextView.text = "6"
-
             rollDice()
         }
     }
@@ -27,8 +22,17 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
